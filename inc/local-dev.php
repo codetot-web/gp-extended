7fe8c3a975a1991e829a2d1ab1897e4a52fcc16b
Khoi Nguyen <khoi@codetot.com>
2023-09-30 17:08:55 +0700

    Fix logic local-dev, add check if function exists

--- inc/local-dev.php
+++ inc/local-dev.php
@@ -9,6 +9,11 @@
 
 $environment_type = wp_get_environment_type() ?? 'production';
 
+/**
+ * Get list of live plugins which need to disable on local environment
+ *
+ * @return array
+ */
 function gp_extended_get_live_plugins() {
     $default_plugins = array(
 		'litespeed-cache/litespeed-cache.php',
@@ -26,20 +31,29 @@ function gp_extended_get_live_plugins() {
     return apply_filters('gp_extended_disable_live_plugins', $default_plugins);
 }
 
-/**
- * Disable live plugins on local/development environment
- *
- * @return void
- */
-function gp_extended_disable_live_plugins() 
-{
-	$production_plugins = gp_extended_get_live_plugins();
+if ( ! function_exists('gp_extended_disable_live_plugins' ) && in_array( $environment_type, array( 'local', 'development' ), true ) ) {
+	/**
+	 * Disable live plugins on local/development environment
+	 */
+	function gp_extended_disable_live_plugins() 
+	{
+		$production_plugins = gp_extended_get_live_plugins();
 
-    if ( !empty($production_plugins) ) :
-    	deactivate_plugins( $production_plugins );
-    endif;
+		if ( !empty($production_plugins) ) :
+			deactivate_plugins( $production_plugins );
+		endif;
+	}
+
+	add_action( 'init', 'gp_extended_disable_live_plugins', 1000 );
 }
 
-if ( in_array( $environment_type, array( 'local', 'development' ), true ) ) {
-	add_action( 'init', 'gp_extended_disable_live_plugins', 1000 );
+if ( ! function_exists('gp_extended_is_local_env' ) ) {
+	/**
+	 * Check if is localhost:3000 with child theme
+	 *
+	 * @return bool
+	 */
+	function gp_extended_is_local_env() {
+		return ! empty( $_SERVER['HTTP_X_CODETOT_CHILD_THEME_HEADER'] ) && 'development' === $_SERVER['HTTP_X_CODETOT_CHILD_THEME_HEADER'];
+	}
 }
